71d9516e6ed0f4c51365dbbef635858162e057f0
Neil Weidinger <neilweidingern@gmail.com>
2022-03-04 18:07:30 +0000

    Implement latency incurring fibonacci (recursive async functions get boxed at the moment, not ideal)

--- examples/fibonacci.rs
+++ examples/fibonacci.rs
@@ -1,3 +1,18 @@
+use async_io::Timer;
+use async_recursion::async_recursion;
+use clap::Parser;
+use std::time::Duration;
+
+#[derive(Parser)]
+struct Args {
+    #[clap(short, long)]
+    n: u32,
+    #[clap(short, long)]
+    hide_latency: bool,
+    #[clap(short, long)]
+    latency_ms: u64,
+}
+
 trait Joiner {
     #[must_use]
     fn is_parallel() -> bool;
@@ -52,30 +67,61 @@ impl Joiner for Parallel {
 }
 
 #[must_use]
-fn fib<J: Joiner>(n: u32) -> (u32, u32) {
+fn fib<J: Joiner>(n: u32, latency: u64) -> (u32, u32) {
     if n <= 1 {
         return (n, 1);
     }
 
-    if n < 10 && J::is_parallel() {
-        return fib::<Serial>(n); // cross over to serial execution
+    // if n < 10 && J::is_parallel() {
+    //     return fib::<Serial>(n); // cross over to serial execution
+    // }
+
+    // incur latency here
+    std::thread::sleep(Duration::from_millis(latency));
+
+    let (ra, rb) = J::join(|| fib::<J>(n - 1, latency), || fib::<J>(n - 2, latency));
+
+    (ra.0 + rb.0, ra.1 + rb.1 + 1)
+}
+
+fn fib_latency(n: u32, latency: u64) -> (u32, u32) {
+    if n <= 1 {
+        return (n, 1);
     }
 
-    let (ra, rb) = J::join(|| fib::<J>(n - 1), || fib::<J>(n - 2));
+    let (ra, rb) = rayon::join_async(
+        fib_latency_helper(n - 1, latency),
+        fib_latency_helper(n - 2, latency),
+    );
+
+    (ra.0 + rb.0, ra.1 + rb.1 + 1)
+}
+
+#[async_recursion]
+async fn fib_latency_helper(n: u32, latency: u64) -> (u32, u32) {
+    if n <= 1 {
+        return (n, 1);
+    }
+
+    // incur latency here
+    Timer::after(Duration::from_millis(latency)).await;
+
+    let (ra, rb) = rayon::join_async(
+        fib_latency_helper(n - 1, latency),
+        fib_latency_helper(n - 2, latency),
+    );
 
     (ra.0 + rb.0, ra.1 + rb.1 + 1)
 }
 
 fn main() {
-    let mut args = std::env::args();
+    let args = Args::parse();
 
-    if args.len() > 2 {
-        eprintln!(r#"Usage: ./rayon_test n"#);
-        return;
-    }
-
-    let n = args.nth(1).unwrap_or("25".to_string()).parse().unwrap();
-    let (fib, calls) = fib::<Parallel>(n);
+    let (fib, calls) = if args.hide_latency {
+        fib_latency(args.n, args.latency_ms)
+    } else {
+        fib::<Parallel>(args.n, args.latency_ms)
+    };
 
     println!("result: {} calls: {}", fib, calls);
 }
